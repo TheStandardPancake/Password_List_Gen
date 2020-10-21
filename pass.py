803b6db21e4d296927a425872b6a519831d73e17
Boyd K <herobrine75@live.com>
2020-10-21 18:44:45 +1100

    numbers sorted and words combinations also sorted

--- pass.py
+++ pass.py
@@ -1,5 +1,9 @@
 #This was written by Boyd Kirkman for educational and professional use only, I take no responsibility for its miss use.
 
+#~~~~~~~~~~~~~~~~~~~~~~~~~~~~Imports~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+from itertools import permutations, combinations
+
+
 #~~~~~~~~~~~~~~~~~~~~~~~~defining lists and vaiables~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 words = ["password"]
 numbers = []
@@ -8,9 +12,20 @@ savelist = []
 #~~~~~~~~~~~~~~~~~~~functions that generate the variants~~~~~~~~~~~~~~~~~~~~~~~~
 def numbers():
     #this will create multiple number combinations
+    num = []
+    for f in range(1,10):
+        num.append(["".join(list(str(p))) for p in combinations([0,1,2,3,4,5,6,7,8,9],f)])
+    num.append(["".join(list(str(p))) for p in combinations([0,1,2,3,4,5,6,7,8,9])])
+    return num
 
-def simple(x):
+def simple(in_numbers,in_words):
     #do just the usual mixing and mashing of the words, words with numbers, etc, these can then be transmuted via the others
+    listw = []
+    listc = []
+    listw.append(in_words)
+    for f in range(1,(len(in_words)+1)):
+        listw.append(["".join(p) for p in combinations(in_words,f)])
+    listc.append(listw.append())
 
 def caps():
     #mix the capital letters throughout
